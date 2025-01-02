7a5bf487935b9daf008ec3570101eea1f05ce2c2
RaiSheng <dryadf16088@gmail.com>
2025-01-03 05:14:51 +0800

    訂單功能-完善功能，檢查user是否存在，扣除商品庫存

--- springboot-mall/src/main/java/com/rswang/springbootmall/dao/impl/ProductDaoImpl.java
+++ springboot-mall/src/main/java/com/rswang/springbootmall/dao/impl/ProductDaoImpl.java
@@ -119,6 +119,20 @@ public class ProductDaoImpl implements ProductDao {
     }
 
     @Override
+    public void updateStock(Integer productId, Integer stock) {
+        String sql = "UPDATE product " +
+                "SET stock = :stock, last_modified_date = :lastModifiedDate " +
+                "WHERE product_id = :productId";
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("stock", stock);
+        params.put("lastModifiedDate", new Date());
+        params.put("productId", productId);
+
+        namedParameterJdbcTemplate.update(sql, params);
+    }
+
+    @Override
     public void deleteProductById(Integer productId) {
         String sql = "DELETE FROM product WHERE product_id = :productId";
 
